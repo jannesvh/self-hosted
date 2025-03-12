07e064f6b87cf5a202133578a376c22c0314662b
Jannes <jvanhelsdingen@retrorabbit.co.za>
2025-03-12 23:26:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,8 +32,6 @@ source install/generate-secret-key.sh
 source install/update-docker-images.sh
 source install/build-docker-images.sh
 source install/bootstrap-snuba.sh
-source install/upgrade-postgres.sh
-source install/set-up-and-migrate-database.sh
 source install/geoip.sh
 source install/setup-js-sdk-assets.sh
 source install/wrap-up.sh
